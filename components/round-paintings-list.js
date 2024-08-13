1839eb7dd05520242afe575db4461657812f2acf
mrdarip <darioelas2005@gmail.com>
2024-08-13 20:27:41 +0200

    completelly changed round island code

---
+++ components/round-paintings-list.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('round-paintings-list', {
+    schema: {
+      list: { type: 'array' }
+    },
+  
+    init: function () {
+      const len = this.data.list.length;
+      for (let i = 0; i <len; i++) {
+        // Create image element and appnd to this entity <a-entity template="src: ./painting.template" data-thumb="#city-thumb"></a-entity>
+        const painting = document.createElement('a-entity');
+        painting.setAttribute('template', 'src: ./templates/painting.template');
+        painting.setAttribute('data-thumb', this.data.list[i]);
+        painting.setAttribute('rotation', "0 "+ (360/len) +" 0");
+
+        this.el.appendChild(painting);
+
+        console.log("painting", painting);
+      }
+    },
+  });
