d4b440778307141078294f1a3888eabf0fe1a0f8
mrdarip <darioelas2005@gmail.com>
2024-08-14 14:20:08 +0200

    fixed sections layout

--- components/art-section.js
+++ components/art-section.js
@@ -8,15 +8,19 @@ AFRAME.registerComponent("art-section", {
   },
 
   init: function () {
-    const paintsPerIsle = this.data.paintings.length / this.data.height;
-    for (let i = 0; i < paintsPerIsle; i++) {
+    const maxPaintingsPerIsle = this.data.width/(0.5 + 1); // margin + img width
+    const islesCount = this.data.paintings.length / maxPaintingsPerIsle;
+    
+    for (let i = 0; i < islesCount; i++) {
       const isle = document.createElement("a-entity");
       isle.setAttribute("template", "src: ./templates/isle.template");
       isle.setAttribute("data-height","3");
 
-      isle.setAttribute("data-paintings", "list:"+this.data.paintings.slice(i * paintsPerIsle, (i+1) * paintsPerIsle).join(","));
+      isle.setAttribute("data-paintings", "list:"+this.data.paintings.slice(i * maxPaintingsPerIsle, (i+1) * maxPaintingsPerIsle).join(",")+";");
+      console.log(this.data.paintings.slice(i * maxPaintingsPerIsle, (i+1) * maxPaintingsPerIsle).join(","));
       isle.setAttribute("data-margin", "0.5");
       isle.setAttribute("data-imgwidth", "1");
+      isle.setAttribute("position","0 0 "+(i* this.data.height/islesCount - this.data.height/2));
       this.el.appendChild(isle);
     }
   },
